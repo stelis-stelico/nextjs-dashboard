da6473b49a52eef4335dc120d1eeae8b21d1dfc1
stelis-stelico <stelisstelico@gmail.com>
2026-03-07 15:06:59 +0100

    ..

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,7 +23,6 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
 
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
